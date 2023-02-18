9ce0faa4f9b181cec1e04f6a4110e0b142fb24fb
Roman Permiakov <info@presstomsk.ru>
2023-02-18 18:04:53 +0700

    Created

---
+++ src/helloapp/Program.java
@@ -0,0 +1,42 @@
+package helloapp;
+
+
+
+public class Program {
+	  public static void main(String[] args) {
+		   
+		  try{
+		        int number = -2; 
+		        Validator.NumberValidator(number);
+		        System.out.println(number);		        
+		    }
+		  catch(NonPositiveException ex){
+		         
+			  	System.out.println(NonPositiveException.serialVersionUID);
+		        System.out.println(ex.getMessage());
+		        System.out.println(ex.getNumber());		        
+		    }
+	  }
+}
+
+class NonPositiveException extends Exception{
+	public static final long serialVersionUID = 1201030038691748218L;
+	private int number;
+    public int getNumber(){return number;}
+    
+    public NonPositiveException(String message, int num){     
+        super(message);        
+        number = num;
+    }
+}
+
+class Validator{
+	 
+    public static void NumberValidator(int num) throws NonPositiveException{     
+        
+        if(num<0) throw new NonPositiveException("The number is non-positive", num);        
+       
+    }
+}
+
+
